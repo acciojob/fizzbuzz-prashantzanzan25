d2b1ce9a5cc49709375afba209c741bebce0eeb4
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-19 14:14:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+for (let i = 1; i <= 20; i++) {
+            let output = "";
+
+            // Check for multiples of three
+            if (i % 3 === 0) {
+                output += "Fizz";
+            }
+
+            // Check for multiples of five
+            if (i % 5 === 0) {
+                output += "Buzz";
+            }
+
+            // If not a multiple of three or five, use the number itself
+            if (output === "") {
+                output = i.toString();
+            }
+
+            // Display the result using alert
+            alert(output);
+        }
